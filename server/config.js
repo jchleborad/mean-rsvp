c5dd81ad1d893ae932e5b46734460f84d7f653fc
John Chleborad <jchleborad@gmail.com>
2018-03-21 20:37:52 -0700

    Layout & Global Components

---
+++ server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  AUTH0_DOMAIN: [chleborad.auth0.com], // e.g., kmaida.auth0.com
+  AUTH0_API_AUDIENCE: "http://localhost:8083/api", // e.g., 'http://localhost:8083/api/'
+  MONGO_URI:
+    process.env.MONGO_URI ||
+    "mongodb://<dbuser>:<dbpassword>@ds119449.mlab.com:19449/mean"
+};
+
+
